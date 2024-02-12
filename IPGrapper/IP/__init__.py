dd71ca9a1ac1b7b373a81e2acceaac11da98b022
Moha-hub-geldroid <81656341+Moha-hub-geldroid@users.noreply.github.com>
2024-02-12 20:45:21 +0300

    Update __init__.py

--- IPGrapper/IP/__init__.py
+++ IPGrapper/IP/__init__.py
@@ -5,15 +5,15 @@ from flask_mail import Mail
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = "zTCaZ7VKdgcHwiRDs54dLq45hFiSeh0NExJcFyPxW3Q"
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///users.db"
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get('SQLALCHEMY_DATABASE_URI')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']= True
 app.config['MAIL_SERVER']= 'smtp.googlemail.com'
 app.config['MAIL_PORT']= 587
 app.config['MAIL_USE_TLS']= True
-app.config['MAIL_USERNAME']= 'gashehteam@gmail.com'
-app.config['MAIL_PASSWORD']= 'ceys fkoa zehq aoqh'
-app.config["UPLOAD_FOLDER"] = "uploads"
+app.config['MAIL_USERNAME']= os.environ.get('MAIL_USERNAME')
+app.config['MAIL_PASSWORD']= os.environ.get('MAIL_PASSWORD')
+app.config["UPLOAD_FOLDER"] = os.environ.get('UPLOAD_FOLDER')
 
 
 db = SQLAlchemy(app)
