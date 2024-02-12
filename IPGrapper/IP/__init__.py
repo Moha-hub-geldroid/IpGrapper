ab53f4a5b88ec3ed8cfd331534c70734f913a3ad
Moha-hub-geldroid <81656341+Moha-hub-geldroid@users.noreply.github.com>
2024-02-12 20:53:04 +0300

    Update __init__.py

--- IPGrapper/IP/__init__.py
+++ IPGrapper/IP/__init__.py
@@ -2,7 +2,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
 from flask_login import LoginManager
 from flask_mail import Mail
-
+import os
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
